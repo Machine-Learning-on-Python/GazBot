0f024ef00dbb73c21d2e87e094fa298cd5993dbb
PetrolBot <31823883+PetrolBot@users.noreply.github.com>
2017-09-11 12:17:29 +0300

    token

--- start.py
+++ start.py
@@ -22,7 +22,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://warm-mesa-34035.herokuapp.com/bot")
+    bot.set_webhook(url="https://warm-mesa-34035.herokuapp.com/427664928:AAFo8XppiVi6PJJi_w_3ZN5UGLBYudInYBk")
     return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
